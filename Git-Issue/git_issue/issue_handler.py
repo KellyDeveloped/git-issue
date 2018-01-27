18f9573294bde2e8361983ddab59ab651ce2de4b
Liam Cairns Kelly <liamcairnskelly@gmail.com>
2018-01-27 19:02:52 +0000

    Split out the git workflow to one method to reduce duplicate code

--- Git-Issue/git_issue/issue_handler.py
+++ Git-Issue/git_issue/issue_handler.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import Callable
 from pathlib import Path
 from git_manager import GitManager
 from json_utils import JsonConvert
@@ -9,55 +10,63 @@ def _increment_issue_count():
     tracker.increment_issue_count()
     tracker.store_tracker()
 
-def _generate_issue_path(id):
-    return Path.cwd().joinpath(f"{id}/issue.json")
+def _handle_git_flow(action: Callable[[], object or None],
+                     should_push=False, 
+                     generate_index_paths: Callable[[], list]=None):
+    gm = GitManager()
+    gm.pull()
+    gm.load_issue_branch()
+
+    result = action()
+
+    if (should_push and generate_index_paths != None):
+        paths = generate_index_paths()
+        gm.add_to_index(paths)
+
+    gm.unload_issue_branch()
+    
+    if (should_push):
+        gm.push()
+
+    return result
+        
+def _generate_issue_folder_path(id):
+    return Path.cwd().joinpath(id)
+
+def _generate_issue_file_path(id):
+    return Path.cwd().joinpath(f"{_generate_issue_folder_path(id)}/issue.json")
+
+def _generate_issue_comment_path(id):
+    return Path.cwd().joinpath(f"{_generate_issue_folder_path(id)}/comment")
 
 def generate_issue_id():
     tracker = Tracker.obtain_tracker()
     return "{}-{}".format(tracker.ISSUE_IDENTIFIER, (tracker.issue_count + 1))
 
 def does_issue_exist(id):
-    return _generate_issue_path(id).exists()
+    return _generate_issue_file_path(id).exists()
 
 def get_issue(id):
-    try:
-        gm = GitManager()
-        gm.load_issue_branch()
-        gm.pull()
-
-        issue = JsonConvert.FromFile(_generate_issue_path(id))
-
-        gm.unload_issue_branch()
-        return issue
+    try:    
+        return _handle_git_flow(lambda : JsonConvert.FromFile(_generate_issue_file_path(id)))
     except IOError(e):
         return None
 
 def get_all_issues():
-    gm = GitManager()
-    gm.pull()
-    gm.load_issue_branch()
-
     path = Path.cwd()
     dirs = [d for d in path.iterdir() if d.is_dir() and d.match("ISSUE-*")]
     issues = [get_issue(i.parts[-1]) for i in dirs]
 
-    gm.unload_issue_branch()
     return issues
 
 def store_issue(issue, cmd):
-    gm = GitManager()
-    gm.pull()
-    gm.load_issue_branch()
+    def action():
+        path = _generate_issue_file_path(issue.id)
+        JsonConvert.ToFile(issue, path)
+        _increment_issue_count()
+        return [str(path)]
 
-    path = _generate_issue_path(issue.id)
-    JsonConvert.ToFile(issue, path)
-    _increment_issue_count()
-
-    gm.add_to_index([str(path)])
-
-    gm.commit(cmd, issue.id)
-    gm.unload_issue_branch()
-    gm.push()
+    _handle_git_flow(action, True)
 
 def display_issue(issue):
     print (f"Issue ID:\t{issue.id}")
