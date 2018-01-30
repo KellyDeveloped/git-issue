8901c6d96e626e438b05b7eef7f99e84f42bb06d
Liam Cairns Kelly <liamcairnskelly@gmail.com>
2018-01-30 10:00:38 +0000

    added date_utils as a module to a new utils package

--- Git-Issue/git_issue/issue.py
+++ Git-Issue/git_issue/issue.py
@@ -1,18 +1,18 @@
 from gituser import GitUser
-from comment import Comment
-from json_utils import JsonConvert
+from utils.json_utils import JsonConvert
+from utils import date_utils
 
 @JsonConvert.register
 class Issue(object):
-    """This class encapsulates what an issue is, and provides utility methods to """
+    """This class encapsulates what an issue is, and may provide supporting methods for issues."""
 
-    def __init__(self, id=None, summary=None, description=None, comments=[],
-                status=None, assignee=None, reporter=None,
-               subscribers=[], attachments=[]):
+    def __init__(self, id:str=None, date=date_utils.get_date_now(), summary:str=None,
+                description:str=None, status:str="open", assignee:GitUser=None, 
+                reporter:GitUser=None, subscribers=[], attachments=[]):
         self.id = id
+        self.date = date
         self.summary = summary
         self.description = description
-        self.comments:[Comment] = comments
         self.status = status
         self.assignee:GitUser = assignee
         self.reporter:GitUser = reporter
